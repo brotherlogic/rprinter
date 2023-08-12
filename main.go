e38b572f5d03f69648d30ffc6ee862d270f0332d
Simon Tucker <brotherlogic@gmail.com>
2023-08-11 17:24:36 -0700

    More detail in output

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func localPrint(ctx context.Context, lines []string) error {
 
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("error in running command %w (output: %v)", err, string(out))
+		return fmt.Errorf("error in running command %w (output: %v) on %v", err, string(out), handle.Name())
 	}
 
 	log.Printf("Printed %v", handle.Name())
